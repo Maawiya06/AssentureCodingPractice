7175a549bb43e3ab0d00761941f21312ef50c313
Maawiya06 <maawiyaameer001@gmail.com>
2025-10-03 21:24:50 +0530

    commit

--- src/LargestSumofGivenSubArray.java
+++ src/LargestSumofGivenSubArray.java
@@ -2,7 +2,7 @@ public class LargestSumofGivenSubArray {
 
     public static void main(String args[]){
 
-        int[] arr = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = new int[] {2, 3, -8, 7, -1, 2, 3};
         int maxSum = Integer.MIN_VALUE;
         int sum = 0;
 
