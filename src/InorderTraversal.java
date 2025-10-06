423d7d8b3ad73a172fd25e7212e77bd3a929e852
Maawiya06 <maawiyaameer001@gmail.com>
2025-10-06 20:38:35 +0530

    commit

--- src/InorderTraversal.java
+++ src/InorderTraversal.java
@@ -33,6 +33,16 @@ public class InorderTraversal {
         preOrderTraversal(root.right, arr);
     }
 
+    public static void PostOrderTraversal(TreeNode root, List<Integer> arr){
+        if(root == null){
+            return;
+        }
+
+        preOrderTraversal(root.left, arr);
+        preOrderTraversal(root.right, arr);
+        arr.add(root.data);
+    }
+
     public static void main(String[] args) {
         // Sample binary tree
         TreeNode root = new TreeNode(1);
@@ -43,9 +53,10 @@ public class InorderTraversal {
 
         List<Integer> result = new ArrayList<>();
         //inorderTraversal(root, result);
-        preOrderTraversal(root, result);
-
+        //preOrderTraversal(root, result);
+        PostOrderTraversal(root, result);
         //System.out.println("inorder Traversal: " + result);
-        System.out.println("preorder Traversal: " + result);
+        //System.out.println("preorder Traversal: " + result);
+        System.out.println("postorder Traversal: " + result);
     }
 }
